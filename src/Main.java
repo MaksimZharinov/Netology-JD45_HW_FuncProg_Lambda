9e20639a1ee5350713862eb721f3d0f564a9996c
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-06-15 01:48:52 +0700

    update calcculator & main

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        try {
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1, 1);
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя!");
+        } catch (Exception e) {
+            System.out.println("Общий обработчик исключений.");
+        }
+
+//        Программа кидает ошибку типа ArithmeticException, т.к.
+//        в переменной b получается ноль, а на ноль делить нельзя
+    }
+}
